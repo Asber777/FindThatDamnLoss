596ddb67eed9af0a20e5bc7ab7f199d3ade82b66
Asber777 <1402051732@qq.com>
2021-09-13 08:59:45 +0000

    OpNode and CompositeLoss partly complete and test OK

--- advertorch/loss.py
+++ advertorch/loss.py
@@ -3,15 +3,15 @@ import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
 from advertorch import oplib
 from advertorch.utils import clamp
-from advertorch.oplib import *
+from advertorch.oplib import getMNISTop
 
-class MyCEloss(_Loss):
-    """CEloss which constracted by my oplib"""
-    def __init__(self, size_average=None, reduce=None,reduction='elementwise_mean'):
-        super(MyCEloss, self).__init__(size_average, reduce, reduction)
+# class MyCEloss(_Loss):
+#     """CEloss which constracted by my oplib"""
+#     def __init__(self, size_average=None, reduce=None,reduction='elementwise_mean'):
+#         super(MyCEloss, self).__init__(size_average, reduce, reduction)
 
-    def forward(self, logits, targets):
-        return myceloss(logits, targets, reduction=self.reduction)
+#     def forward(self, logits, targets):
+#         return myceloss(logits, targets, reduction=self.reduction)
 
 class ZeroOneLoss(_Loss):
     """Zero-One Loss"""
@@ -67,31 +67,25 @@ class SoftLogitMarginLoss(_Loss):
 '''
 Create a LOSS graph .
 input logits number ->Z
-Number of T2T/v2v after Logits or vector -> M 
+Number of T2T/v2v after Logits or vector -> M
 Number of v2v after 2v2v(m) ->N
 abbreviation - T2T:T ; T2v:t ; 2v2v:m ; v2v:v
 ====================Example=====================
 if K = 4 M = 2 N = 1, then the structure of Loss may looks like:
-Logits->T2T(1)->T2T(5)->T2v(09)->v2v->v2v-\__2v2v->v2v--\
-Logits->T2T(2)->T2T(6)->T2v(10)->v2v->v2v-/              \__2v2v->v2v->LOSS
-Logits->T2T(3)->T2T(7)->T2v(11)->v2v->v2v-\__2v2v->v2v---/
-Logits->T2T(4)->T2T(8)->T2v(12)->v2v->v2v-/
-For convenient,I use this form's merge
-v1--
-     \
-v2--(v6)-v2v
-         \
-v3-------(v7）-v2v
-                 \
-v4--------------(v8)-v2v
-                       \
-v5--------------------(v9)--v2v--Loss 我觉得这还是和上面那种形式有差别的，前者所有branch的op数都一样，但是后者明显branch5在merge操作的op数变少了
+Logits->T2T(1)->T2T(5)->T2v(09)->v2v->v2v->\
+Logits->T2T(2)->T2T(6)->T2v(10)->v2v->v2v->2v2v->v2v->\
+Logits->T2T(3)->T2T(7)->T2v(11)->v2v->v2v->\          |
+Logits->T2T(4)->T2T(8)->T2v(12)->v2v->v2v->2v2v->v2v->2v2v->v2v->LOSS
 '''
 class OpNode():
+    #FOR VISILAZTION
+    LINK = "<-"
+    MERGE = "/<-"
     def __init__(self, type=None, op=None, lchild=None, rchild=None):
-        assert type in ['T','v','t','m','leaf']
+        assert type in ['T','v','t','m','leaf','loss']
+        self.type = type
         self.op,self.lchild,self.rchild = op,lchild,rchild
-        self.name = op.__name__
+        self.name = type if type=='leaf' or type=='loss' else op.__name__[:3]
         self.result = 0
     # We assume that if op type is T v t, than it only has input from lchild
     def forward(self, logits, label=None):
@@ -105,7 +99,7 @@ class OpNode():
             self.result = self.op(x)
         if self.type=='t':#T2v input:Tensor
             Z = self.lchild.forward(logits)
-            assert isinstance(Z,torch.Tensor) and len(Z.shape)==2
+            assert isinstance(Z,torch.Tensor) and len(Z.shape)==1
             self.result = self.op(Z,self.label)
         if self.type=='m':#tv2v input:vector vector
             x = self.lchild.forward(logits,label)
@@ -114,9 +108,10 @@ class OpNode():
             self.result = self.op(x,y)
         if self.type=='leaf':
             self.result = logits.clone().detach()
+        if self.type=='loss':
+            self.result = self.lchild.forward(logits,label)
+            assert isinstance(Z,torch.Tensor) and len(Z.shape)==1
         return self.result
-    def visualization(self):
-        
 
 # Assume that all op need not keep requires_grad same as input tensor/vector
 class CompositeLoss(_Loss):
@@ -141,52 +136,61 @@ class CompositeLoss(_Loss):
 
     def getLoss(self,loss):
         assert self._checkLegal(loss)
-        self.branch = [[None] for _ in range(self.K)]
+        branch = [[OpNode('leaf')] for _ in range(self.K)]
         ahead = self.K *(2*self.M+1) # branch op num
-        for i in range(ahead):
+        for i in range(ahead):# by this way ,you can actually Transfer to vector immediately after leaf,as the code here is unlimited.
             type = loss[i*3]
             op = self.op[type][int(loss[i*3+1:i*3+3])]
-            self.branch[i%self.K].append(OpNode(type,op,self.branch[i%self.K][-1],None))
-        self.merge = [self.branch[k][-1] for k in range(self.K)]
-        leftnode = False
-        layer_node_num = self.K
-        while(layer_node_num!=1):
+            node = OpNode(type,op,branch[i%self.K][-1],None)
+            branch[i%self.K].append(node)
+        nodes2bmerge = [branch[k][-1] for k in range(self.K)]
+        layer_node_num, leftnode = self.K, False
+        while(layer_node_num!=1 or leftnode!=False):
             if not leftnode:#no node left to merge
                 layer_node_num,leftnode = layer_node_num//2,False if layer_node_num%2==0 else True
             elif leftnode and layer_node_num%2==0:#a node left to merge and the num of nodes product by last layer is even
                 layer_node_num,leftnode = layer_node_num//2,True
-            elif leftnode and layer_node_num%2:
+            elif leftnode and layer_node_num%2==1:
                 layer_node_num,leftnode = layer_node_num//2 + 1,False
-            # According to layer_node_num ,create node and add to self.merge
-            temp_merge = []
-            for j in range(layer_node_num):
-                i += 1
-                type = loss[i*3]
+            # According to layer_node_num:how much pair node to be merged, create next layer's nodes and add to self.merge
+            next_nodes2bmerge = []
+            for j in range(layer_node_num):#product one layer with merge operations
+                merge_node = []
+                i, type = i+1,'m'
                 op = self.op[type][int(loss[i*3+1:i*3+3])]
-                lchild = self.merge[-j*2-1]
-                rchild = self.merge[-j*2-2]
-                temp_merge.append(OpNode(type,op,lchild,rchild))
-                for _ in range(self.N):
-                    i += 1
-                    type = loss[i*3]
+                node = OpNode(type,op,nodes2bmerge[j*2],nodes2bmerge[j*2+1])#???
+                merge_node.append(node)
+                for _ in range(self.N): #handle N v2vnode after tv2vnode
+                    i,type= i+1,'v'
                     op = self.op[type][int(loss[i*3+1:i*3+3])]
-                    lchild = temp_merge[-1]
-                    temp_merge.append(OpNode(type,op,lchild,None))
-            self.merge = self.merge + temp_merge
-        return self.merge[-1]
-
+                    lchild = merge_node[-1]
+                    node = OpNode(type,op,lchild,None)
+                    merge_node.append(node)
+                    # print("add {}{}node {}".format(node.type,node.name,node))
+                next_nodes2bmerge.append(merge_node[-1])
+            #does it affect if it's position changed? Yes, after I change it here, result turn right
+            if leftnode: next_nodes2bmerge.append(nodes2bmerge[-1])
+            nodes2bmerge = next_nodes2bmerge
+        assert len(nodes2bmerge)==1
+        self.loss = OpNode('loss',op=None,lchild=nodes2bmerge[0],rchild=None)
+        return self.loss
+    def visualization(self):
+        pass
     # TODO random constract loss
     def randomLossstr(self):
         pass
 
     def forward(self, logits, targets):
-        return 
+        return self.loss.forward(logits,targets)
 
 v2v,t2t,tv2v,t2v,_ = getMNISTop()
 cl = CompositeLoss(t2t,t2v,v2v,tv2v,K=5,M=1,N=1)
 loss = 'T01T02T01T02T01t03t04t03t04t03v05v06v05v06v05m07v08m07v08m07v08m07v08'
 loss = cl.getLoss(loss)
-
+r = loss
+while(r!=None):
+    print(r)
+    r = r.lchild
 
 
 def zero_one_loss(input, target, reduction='elementwise_mean'):
@@ -237,28 +241,12 @@ def soft_logit_margin_loss(
     return _reduce_loss(loss, reduction) + offset
 
 
-def myceloss(Z,label,reduction='elementwise_mean'):
-    opdict,_ = getOP()
-    U,B,M,S = opdict['Unary'],opdict['Binary'],opdict['Multinary'],opdict['SpecialMulti']
-    zy = S['getLabellogit'](Z,label)
-    negzy = U['reverse'](zy)
-    logsumexpZ = U['logarithm'](M['Sum'](M['Exponential'](Z)))
-    # which is already in torch "torch.logsumexp"
-    loss = B['addtensor'](negzy,logsumexpZ)
-    return _reduce_loss(loss, reduction)
-
-
-def elementwise_margin(logits, label):
-    batch_size = logits.size(0)
-    topval, topidx = logits.topk(2, dim=1)
-    maxelse = ((label != topidx[:, 0]).float() * topval[:, 0]
-               + (label == topidx[:, 0]).float() * topval[:, 1])
-    return maxelse - logits[torch.arange(batch_size), label]
-
-
-# TODO random constract loss
-def random_loss(input, target, reduction='elementwise_mean'):
-    '''assume all data are the same class and the target is all groundtruth.'''
-    loss = None
-    return _reduce_loss(loss, reduction)
-
+# def myceloss(Z,label,reduction='elementwise_mean'):
+#     opdict = getOP()
+#     U,B,M,S = opdict['Unary'],opdict['Binary'],opdict['Multinary'],opdict['SpecialMulti']
+#     zy = S['getLabellogit'](Z,label)
+#     negzy = U['reverse'](zy)
+#     logsumexpZ = U['logarithm'](M['Sum'](M['Exponential'](Z)))
+#     # which is already in torch "torch.logsumexp"
+#     loss = B['addtensor'](negzy,logsumexpZ)
+#     return _reduce_loss(loss, reduction)
--- advertorch/oplib.py
+++ advertorch/oplib.py
@@ -1,175 +1,149 @@
-'''make sure that b must be a number (float tensor with shape [1])'''
-from math import exp
-from numpy.lib.arraysetops import isin
+'''
+operation lib and create a loss_space
+make sure that b must be a number (float tensor with shape [1])
+'''
+# TODO 测试代码加上检测是否会使得tensor/vector输出无梯度 ->貌似并不需要,loss只需要计算数值
 import torch as t
 import torch.nn.functional as F
-TestOP = False
+TestOP = True#Test OK -9/11
 
-'''Unary Operation: input a vector; return a vector'''
-zero = lambda x,b=0:t.zeros_like(x)
-one = lambda x,b=0:t.ones_like(x)
-constant = lambda x,b=1:t.ones_like(x)*b
-addconstant = lambda x,b=0:x+b
-remain = lambda x,b=0:x
-reverse = lambda x,b=0:-x+b
-absolute = lambda x,b=0:abs(x)+b
-square = lambda x,b=2:x**b
-reciprocal = lambda x,b=0:1/(x+b)
-multiply = lambda x,b=1:b*x
-exponential = lambda x,b=0:t.exp(x+b)
-logarithm = lambda x,b=0:t.log(x+b)
-cosh = lambda x,b=1:t.cosh(x)*b
-sinh = lambda x,b=1:t.sinh(x)*b
-# TODO : sinc = lambda
-maxima = lambda x,b=0:t.clamp(x,min=b)
-minima = lambda x,b=0:t.clamp(x,max=b)
-sigmoid = lambda x,b=0:t.sigmoid(x+b)
-erf = lambda x,b=0:t.erf(x+b)
-ceil = lambda x,b=0:t.ceil(x)+b
-sign = lambda x,b=0:t.sign(x+b)
-trunc = lambda x,b=0:t.trunc(x+b)
-softsign = lambda x,b=0:F.softsign(x)
-softplus = lambda x,b=1:t.log(b+t.exp(x))
-swish = lambda x,b=1.702:x*sigmoid(b*x)
+'''v2vOP: input a vector; return a vector'''
+def zero(x,b=0):return t.zeros_like(x)
+def one(x,b=0):return t.ones_like(x)
+def constant(x,b=0.5):return t.ones_like(x)*b
+def addconstant(x,b=1):return x+b
+def remain(x,b=0):return x
+def reverse(x,b=0):return -x+b
+def absolute(x,b=0):return abs(x)+b
+def square(x,b=2):return x**b
+def reciprocal(x,b=0):return 1/(x+b)
+def multiply(x,b=2):return b*x
+def exponential(x,b=0):return t.exp(x+b)
+def logarithm(x,b=0):return t.log(abs(x+b))# Use abs to makesure no bug
+def cosh(x,b=1):return t.cosh(x)*b
+def sinh(x,b=1):return t.sinh(x)*b
+def maxima(x,b=0):return t.clamp(x,min=b)
+def minima(x,b=0):return t.clamp(x,max=b)
+def sigmoid(x,b=0):return t.sigmoid(x+b)
+def erf(x,b=0):return t.erf(x+b)
+def ceil(x,b=0):return t.ceil(x)+b
+def sign(x,b=0):return t.sign(x+b)
+def trunc(x,b=0):return t.trunc(x+b)
+def softsign(x,b=0):return F.softsign(x)
+def softplus(x,b=1):return t.log(b+t.exp(x))
+def swish(x,b=1.702):return x*sigmoid(b*x)
 
-UnaryList = ["zero","one","constant","addconstant","remain","reverse","absolute","square","reciprocal","multiply","exponential",\
-    "logarithm","cosh","sinh","maxima","minima","sigmoid","erf","ceil","sign","trunc","softsign","softplus","swish"]
-UnaryDict = {"zero":zero,"one":one,"constant":constant,"addconstant":addconstant,"remain":remain,"reverse":reverse,"absolute":absolute,\
-    "square":square,"reciprocal":reciprocal,"multiply":multiply,"exponential":exponential,"logarithm":logarithm,"cosh":cosh,"sinh":sinh,\
-        "maxima":maxima,"minima":minima,"sigmoid":sigmoid,"erf":erf,"ceil":ceil,"sign":sign,"trunc":trunc,"softsign":softsign,\
-            "softplus":softplus,"swish":swish}
-
-def _testUnary(labelnum,unarylist):
-    a = t.randn(labelnum)
-    print(a)
-    print("============a===========")
-    for op in unarylist:
-        print(op)
-        r = eval(op)(a)
-        print(r)
-        assert r.shape[0] == labelnum
-        print("--------------")
-if TestOP: _testUnary(10,UnaryList)
-
-
-
-
-
-
-'''Binary Operation:input two vectors; return a vector'''
-addtensor = lambda x,y,b=0:x+y
-multitensor = lambda x,y,b=0:x*y
-subtensor = lambda x,y,b=0:x-y
-divtensor = lambda x,y,b=0:t.divide(x,y)
-maxtensor = lambda x,y,b=0:t.max(t.stack([x,y]),0)[0]
-mintensor = lambda x,y,b=0:t.min(t.stack([x,y]),0)[0]
-sigmoidtensor = lambda x,y,b=0:y*t.sigmoid(x)
-subsqexp = lambda x,y,b=1:t.exp(-b*(x-y)**2)
-subabsexp = lambda x,y,b=1:t.exp(-b*abs(x-y))
-firstord = lambda x,y,b=0.5:b*x+(1-b)*y
-
-BinaryList = ["addtensor","multitensor","subtensor","divtensor","maxtensor","mintensor","sigmoidtensor","subsqexp","subabsexp","firstord"]
-BinaryDict = {"addtensor":addtensor,"multitensor":multitensor,"subtensor":subtensor,"divtensor":divtensor,"maxtensor":maxtensor,\
-    "mintensor":mintensor,"sigmoidtensor":sigmoidtensor,"subsqexp":subsqexp,"subabsexp":subabsexp,"firstord":firstord}
-def _testBinary(labelnum,binarylist):
-    a = t.randn(labelnum)
-    b = t.randn(labelnum)
-    print(a)
-    print("===========a============")
-    print(b)
-    print("===========b============")
-    for op in binarylist:
-        print(op)
-        r = eval(op)(a,b)
-        print(r)
-        assert r.shape[0] == labelnum
-        print("--------------")
-if TestOP: _testBinary(10,BinaryList)
-
-
-
-
-
-
-
-'''Multinary'''
-''''SpecialMultiary: input logits tensor; return a Return specail logit vector'''
-def Maxelse(Z,label):
-    topval, topidx = Z.topk(2, dim=1)
-    maxelse = ((label != topidx[:, 0]).float() * topval[:, 0]
-               + (label == topidx[:, 0]).float() * topval[:, 1])
-    return maxelse
-def Minelse(Z,label):
-    label_num = Z.shape[1]
-    minval, minidx = Z.topk(label_num, dim=1)
-    minelse = ((label != minidx[:,label_num-1]).float() * minval[:, label_num-1]
-               + (label == minidx[:,label_num-1]).float() * minval[:, label_num-2])
-    return minelse
-def Labelelse(Z,label,n):
-    batchsize,label_num = Z.shape
-    assert n<label_num and n>0
-    newZ = t.stack([t.cat((Z[i][:label[i]],Z[i][label[i]+1:])) for i in range(batchsize)])
-    topval, _ = newZ.topk(label_num-1, dim=1)
-    return topval[:,n-1]
-getLabellogit = lambda Z,label:Z[t.arange(len(label)), label]
-def Sum(Z,b=1):return t.sum(Z,dim=1)*b
-
-'''Multinary opration:input a logits tensor and return a tensor with the same shape'''
-def WeightZ(Z,weight):
-    assert isinstance(weight,t.Tensor)
-    assert weight.shape[0]==Z.shape[1]
-    return Z*weight
-# - - - - The Multinary OP below is overall version of Unary OP - - - -
-Dividenormal = lambda Z,b=1:Z/b
-Multipnormal = lambda Z,b=1:Z*b 
-def Addconstant(Z,b=0):return Z+b
+'''T2T:input a logits Tensor and return a Tensor with the same shape'''
+def Zero(Z,b=0):return t.zeros_like(Z)
+def One(Z,b=0):return t.ones_like(Z)
+def Constant(Z,b=0.5):return t.ones_like(Z)*b
+def Addconstant(Z,b=1):return Z+b
+def Remain(Z,b=0):return Z
+def Reverse(Z,b=0):return -Z+b
+def Absolute(Z,b=0):return abs(Z)+b
 def Square(Z,b=2):return Z**b
 def Reciprocal(Z,b=0):return 1/(Z+b)
+def Multiply(Z,b=2):return b*Z
 def Exponential(Z,b=0):return t.exp(Z+b)
-def Logarithm(Z,b=0):
-    assert b+min(t.min(Z,dim = 1)[0])>0
-    return t.log(Z+b) #make sure b>0 and b>abs(min(x))
+def Logarithm(Z,b=0):return t.log(abs(Z+b)) #please make sure b>0 and b>abs(min(x))
 def Cosh(Z,b=1):return t.cosh(Z)*b
 def Sinh(Z,b=1):return t.sinh(Z)*b
+def Maxima(Z,b=0):return t.clamp(Z,min=b)
+def Minima(Z,b=0):return t.clamp(Z,max=b)
+def Sigmoid(Z,b=0):return t.sigmoid(Z+b)
 def Erf(Z,b=0):return t.erf(Z+b)
 def Ceil(Z,b=0):return t.ceil(Z+b)
 def Sign(Z,b=0):return t.sign(Z+b)
 def Trunc(Z,b=0):return t.trunc(Z+b)
 def Softsign(Z,b=0):return F.softsign(Z+b)
+def Softplus(Z,b=1):return t.log(b+t.exp(Z))
 def Swish(Z,b=1.702):return Z*sigmoid(b*Z,0)
-'''TripMultinary: input a tensor with 2 number ,return a tensor'''
-def ShiftZ(Z,s=1,m=1):return s*Z+m
-def Softplus(Z,a=1,b=1):return t.log(a+b*t.exp(Z))
 
-TripMultinaryList = ["ShiftZ","Softplus"]
-SpecialMultinaryList = ["WeightZ","Maxelse","Minelse","Labelelse","getLabellogit","Sum"]
-MultinaryList = ["Dividenormal","Multipnormal","Addconstant","Square","Reciprocal","Exponential","Logarithm","Cosh","Sinh","Erf",\
-    "Ceil","Sign","Trunc","Softsign","Swish"]
-TripMultinaryDict = {"ShiftZ":ShiftZ,"Softplus":Softplus}
-SpecialMultinaryDict = {"WeightZ":WeightZ,"Maxelse":Maxelse,"Minelse":Minelse,"Labelelse":Labelelse,"getLabellogit":getLabellogit,"Sum":Sum}
-MultinaryDict = {"Dividenormal":Dividenormal,"Multipnormal":Multipnormal,"Addconstant":Addconstant,"Square":Square,\
-    "Reciprocal":Reciprocal,"Exponential":Exponential,"Logarithm":Logarithm,"Cosh":Cosh,"Sinh":Sinh,"Erf":Erf,\
-    "Ceil":Ceil,"Sign":Sign,"Trunc":Trunc,"Softsign":Softsign,"Swish":Swish}
-def _testMutinary(batchsize,labelnum,List):
+v2vlist = [zero,one,constant,addconstant,remain,reverse,absolute,square,reciprocal,multiply,exponential,\
+    logarithm,cosh,sinh,maxima,minima,sigmoid,erf,ceil,sign,trunc,softsign,softplus,swish]
+T2Tlist = [Zero,One,Constant,Addconstant,Remain,Reverse,Absolute,Square,Reciprocal,Multiply,Exponential,\
+    Logarithm,Cosh,Sinh,Maxima,Minima,Sigmoid,Erf,Ceil,Sign,Trunc,Softsign,Softplus,Swish]
+
+
+def WeightZ(Z,weight=None):
+    if weight==None:return Z
+    assert isinstance(weight,t.Tensor)
+    assert weight.shape[0]==Z.shape[1]
+    return Z*weight
+# This op is not include in our RL demo v0.1
+
+#2v2v op:input 2 vector output 1 vector
+def addtensor(x,y,b=0):return x+y
+def multitensor(x,y,b=0):return x*y
+def subtensor(x,y,b=0):return x-y
+def divtensor(x,y,b=0):return t.divide(x,y)
+def maxtensor(x,y,b=0):return t.max(t.stack([x,y]),0)[0]
+def mintensor(x,y,b=0):return t.min(t.stack([x,y]),0)[0]
+def sigmoidtensor(x,y,b=0):return y*t.sigmoid(x)
+def subsqexp(x,y,b=1):return t.exp(-b*(x-y)**2)
+def subabsexp(x,y,b=1):return t.exp(-b*abs(x-y))
+def firstord(x,y,b=0.5):return b*x+(1-b)*y
+
+#T2v op : input a logits tensor and a label ; return a logit vector which target label
+def Top1else(Z,label,n=0):
+    topval, topidx = Z.topk(2, dim=1)
+    maxelse = ((label != topidx[:, 0]).float() * topval[:, 0]
+               + (label == topidx[:, 0]).float() * topval[:, 1])
+    return maxelse
+def Minelse(Z,label,n=0):
+    label_num = Z.shape[1]
+    minval, minidx = Z.topk(label_num, dim=1)
+    minelse = ((label != minidx[:,label_num-1]).float() * minval[:, label_num-1]
+               + (label == minidx[:,label_num-1]).float() * minval[:, label_num-2])
+    return minelse
+def TopNelse(Z,label,n):
+    batchsize,label_num = Z.shape
+    assert n<label_num and n>0
+    newZ = t.stack([t.cat((Z[i][:label[i]],Z[i][label[i]+1:])) for i in range(batchsize)])
+    topval, _ = newZ.topk(label_num-1, dim=1)
+    return topval[:,n-1]
+def getLabellogit(Z,label,n=0):return Z[t.arange(len(label)), label]
+def Sum(Z,label=None,n=1):return t.sum(Z,dim=1)*n
+
+
+tv2vlist = [addtensor,multitensor,subtensor,divtensor,maxtensor,mintensor,sigmoidtensor,subsqexp,subabsexp,firstord]
+T2vlist = [Top1else,Minelse,TopNelse,getLabellogit,Sum]
+
+
+def getMNISTop():
+    def Top2else(Z,label):return TopNelse(Z,label,2)
+    def Top3else(Z,label):return TopNelse(Z,label,3)
+    def Top4else(Z,label):return TopNelse(Z,label,4)
+    def Top5else(Z,label):return TopNelse(Z,label,5)
+    def Top6else(Z,label):return TopNelse(Z,label,6)
+    def Top7else(Z,label):return TopNelse(Z,label,7)
+    def Top8else(Z,label):return TopNelse(Z,label,8)
+    v2vlist = [zero,one,constant,addconstant,remain,reverse,absolute,square,reciprocal,multiply,exponential,\
+    logarithm,cosh,sinh,maxima,minima,sigmoid,erf,ceil,sign,trunc,softsign,softplus,swish]
+    T2Tlist = [Zero,One,Constant,Addconstant,Remain,Reverse,Absolute,Square,Reciprocal,Multiply,Exponential,\
+    Logarithm,Cosh,Sinh,Maxima,Minima,Sigmoid,Erf,Ceil,Sign,Trunc,Softsign,Softplus,Swish]
+    tv2vlist = [addtensor,multitensor,subtensor,divtensor,maxtensor,mintensor,sigmoidtensor,subsqexp,subabsexp,firstord]
+    T2vlist = [Top1else,Top2else,Top3else,Top4else,Top5else,Top6else,Top7else,Top8else,Minelse,getLabellogit,Sum]
+    opdict = {'v2v':v2vlist,'2v2v':tv2vlist,'T2T':T2Tlist,'T2v':T2vlist}
+    return v2vlist,T2Tlist,tv2vlist,T2vlist,opdict
+
+# - - For test - - - - -- - - - - - - - - - -- 
+# TODO : modify ,cause I'm not done with reconstructing this function
+def _test(batchsize,labelnum,T2Tlist,T2vlist,v2vlist,tv2vlist):
     logits = abs(t.randn(batchsize,labelnum))
+    a = t.randn(labelnum)
+    b = t.randn(labelnum)
     print(logits)
     print("===========logits============")
-    for op in List:
+    print(a)
+    print("==============a==============")
+    print(b)
+    print("==============b==============")
+    for op in T2Tlist:
         print(op)
-        r = eval(op)(logits)
+        r = op(logits)
         print(r)
         assert r.shape.numel()==batchsize*labelnum
+        assert r.shape[0] == labelnum
         print("--------------")
-if TestOP:
-    _testMutinary(10,10,MultinaryList)
-
-def getOP(intro = False):
-    opdict = {'Unary':UnaryDict,'Binary':BinaryDict,'Multinary':MultinaryDict,'TripMulti':TripMultinaryDict,\
-        'SpecialMulti':SpecialMultinaryDict}
-    oplist = [UnaryList,BinaryList,MultinaryList,TripMultinaryList,SpecialMultinaryList]
-    if intro:
-        opassemble_name = ["Unary","Bianry","Multinary","TripMulti","SpecialMulti"]
-        for i,list in enumerate(oplist):
-            print("{}:".format(opassemble_name[i]))
-            print(list)
-    return opdict,oplist
+    
